05c70d9c37e7407d7e8a06330dff7fa3778d7366
AngelicaRojas <angelicamrs16@gmail.com>
2018-05-29 20:21:50 -0600

    index.php

--- index.php
+++ index.php
@@ -103,14 +103,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<h1><a href="index.php"><span> </span>Nutri-Fitness</a></h1>
 			</div>
 			<div class="w3ls_logo_products_left1">
-				<ul class="special_items">
-					<!--<li><a href="events.html">Events</a><i>/</i></li>-->
-					<li><a href="about.html">Nosotros</a><i>/</i></li>
-					<li><a href="products.html">Best Deals</a><i>/</i></li>
-					<li><a href="services.html">Servicios</a></li>
-				</ul>
-			</div>
-			<div class="w3ls_logo_products_left1">
 				<ul class="phone_email">
 					<li><i class="fa fa-phone" aria-hidden="true"></i>2234 6567</li>
 					<li><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:store@grocery.com">nutrifitness@gmail.com</a></li>
